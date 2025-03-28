9df5f680ae2917bb62d21a8d39ee10ad2a7150d9
Random-Bee <co21btech11002@iith.ac.in>
2025-03-28 15:16:15 +0530

    o2pl init

---
+++ 1.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+using namespace chrono;
+typedef long long ll;
+
+// Enum for read and  write
+enum class Operation {
+    READ,
+    WRITE
+};
+
+class Transaction {
+public:
+    ll id;
+    // Map of item id to pair of operatin id and operation type
+    map<ll, pair<ll, Operation>> operations;
+};
+
+class Item {
+public:
+    atomic<ll> read_op_ctr, write_op_ctr, read_ulock_ctr, write_ulock_ctr;
+    ll val;
+    Item() {
+        read_op_ctr = 0;
+        write_op_ctr = 0;
+        read_ulock_ctr = 0;
+        write_ulock_ctr = 0;
+        val = 0;
+    }
+};
+
+class O2PL {
+private:
+
+    vector<Item*> items;
+    ll size;
+
+    ll get_op_ctr(ll item_id, Operation op) {
+        ll op_ctr;
+        if (op == Operation::READ) {
+            op_ctr = items[item_id]->read_op_ctr.fetch_add(1);
+        } else {
+            op_ctr = items[item_id]->write_op_ctr.fetch_add(1);
+        }
+        return op_ctr;
+    }
+
+public:
+    
+    O2PL(ll m) {
+        items.resize(m, nullptr);
+        for (int i = 0; i < m; i++) {
+            items[i] = new Item();
+        }
+        size = m;
+    }
+
+    ~O2PL() {
+        for (int i = 0; i < size; i++) {
+            delete items[i];
+        }
+    }
+
+    void read(Transaction* t, ll item_id, ll& loc) {
+        ll op_ctr = get_op_ctr(item_id, Operation::READ);
+
+        while (op_ctr > items[item_id]->write_op_ctr);
+
+        loc = items[item_id]->val;
+
+        t->operations[item_id] = {op_ctr, Operation::READ};
+
+        items[item_id]->read_op_ctr++;
+    }
+
+    void write(Transaction* t, ll item_id, ll newVal) {
+        ll op_ctr = get_op_ctr(item_id, Operation::WRITE);
+
+        while (op_ctr > max(items[item_id]->write_op_ctr, items[item_id]->read_op_ctr));
+
+        items[item_id]->val = newVal;
+
+        t->operations[item_id] = {op_ctr, Operation::WRITE};
+
+        items[item_id]->write_op_ctr++;
+    }
+
+    void tryCommit(Transaction* t) {
+        for (auto& [item_id, temp] : t->operations) {
+            ll ctr = temp.first;
+            Operation op = temp.second;
+            if (op == Operation::READ) {
+                while (ctr > items[item_id]->write_ulock_ctr);
+            }
+            else {
+                while (ctr > max(items[item_id]->write_ulock_ctr, items[item_id]->read_ulock_ctr));
+            }
+        }
+        for (auto& [item_id, temp] : t->operations) {
+            ll ctr = temp.first;
+            Operation op = temp.second;
+            if (op == Operation::READ) {
+                items[item_id]->read_ulock_ctr++;
+            }
+            else {
+                items[item_id]->write_ulock_ctr++;
+            }
+        }
+    }
+};
+
+int main() {
+}
